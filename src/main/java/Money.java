afbcbdeaa4a6dfeb8afde755df7755838abb9578
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2017-03-01 22:18:56 +0000

    end of chapter 8

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -8,14 +8,23 @@
  *
  * @author ljones
  */
-public class Money {
+abstract class Money {
 
+    abstract Money times(int multiplier);
     protected int amount;
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
-            && getClass().equals(money.getClass());
+                && getClass().equals(money.getClass());
     }
 
 }
