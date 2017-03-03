ee58cdc183f8f5bb2389a7fb2c1cd3c9d24142ad
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2017-03-03 13:22:02 +0000

    Chapter 8 tests passing

--- src/test/java/MultiCurrencyMoneyTest.java
+++ src/test/java/MultiCurrencyMoneyTest.java
@@ -58,7 +58,7 @@ public class MultiCurrencyMoneyTest {
 
     @Test
     public void testFrancMultiplication() {
-        Franc five = Money.franc(5);
+        Money five = Money.franc(5);
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
     }
