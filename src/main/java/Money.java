b0238d105443655eccbd3e4414be52631b43b91b
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2017-03-03 18:11:11 +0000

    Chapter 11

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -12,27 +12,26 @@ class Money {
 
     protected int amount;
     protected String currency;
-    
-    Money times(int multiplier) {
-        return new Money(amount * multiplier, currency);
-    };
-  
+
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
 
     Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
 
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
     String currency() {
         return currency;
-    }
-
-    static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
-    }
-
-    static Money franc(int amount) {
-        return new Franc(amount, "CHF");
     }
 
     public boolean equals(Object object) {
